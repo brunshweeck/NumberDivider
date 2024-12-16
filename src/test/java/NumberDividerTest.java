2bb6d98730f0ddcce658a483f7b179d6d7e7f5e0
brunshweeck <brunshweeck1234@gmail.com>
2024-12-16 12:24:10 +0100

    Find Des Test Préleminaire.

---
+++ src/test/java/NumberDividerTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberDividerTest {
+
+    NumberDivider numberDivider;
+
+    @BeforeEach
+    public void setUp() {
+        numberDivider = new NumberDivider(LocalTime.NOON, LocalTime.MIDNIGHT);
+    }
+
+    @Test
+    public void divideStatusTest() {
+        assertTrue(numberDivider.isActive(LocalTime.NOON));
+    }
+
+    @Test
+    public void divideFiniteNonZeroByZeroTest() {
+        for (int i = 1; i < 10; i++) {
+            assertTrue(Double.isInfinite(numberDivider.divide(i, 0.0)));
+        }
+    }
+
+    @Test
+    public void divideInfiniteByZeroTest() {
+        assertTrue(Double.isInfinite(numberDivider.divide(Double.POSITIVE_INFINITY, 0.0)));
+        assertTrue(Double.isInfinite(numberDivider.divide(Double.NEGATIVE_INFINITY, 0.0)));
+    }
+
+    @Test
+    public void divideNaNByZeroTest() {
+        assertTrue(Double.isNaN(numberDivider.divide(Double.NaN, 0.0)));
+    }
+
+    @Test
+    public void divideZeroByZeroTest() {}
+
+    @Test
+    public void divideByOneTest() {
+        for (int i = 0; i < 10; i++) {
+            assertEquals(numberDivider.divide(i, 1), i);
+        }
+    }
+
+    @Test
+    public void divideFinalNonZeroByItselfTest() {
+        for (int i = 0; i < 10; i++) {
+            assertEquals(numberDivider.divide(i, i), 1);
+        }
+    }
+
+}
