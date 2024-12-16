9b63480d87bc7b5a36e654901737bbe76ca7d863
brunshweeck <brunshweeck1234@gmail.com>
2024-12-16 13:37:38 +0100

    Fin de l'Implementation

--- src/main/java/NumberDivider.java
+++ src/main/java/NumberDivider.java
@@ -1,2 +1,5 @@
 public class NumberDivider {
+    public double divide(double lhs, double rhs) {
+        return lhs / rhs;
+    }
 }
--- src/test/java/NumberDividerTest.java
+++ src/test/java/NumberDividerTest.java
@@ -1,54 +1,75 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberDividerTest {
 
-    NumberDivider numberDivider = new NumberDivider();
+    NumberDivider numberDivider;
 
-//    @Test
-//    public void divideStatusTest() {
-//        assertTrue(numberDivider.isActive(LocalTime.NOON));
-//    }
-
-    @Test
-    public void divideFiniteNonZeroByZeroTest() {
-        for (int i = 1; i < 10; i++) {
-            assertTrue(Double.isInfinite(numberDivider.divide(i, 0.0)));
-        }
+    @BeforeEach
+    void setUp() {
+        numberDivider = new NumberDivider();
     }
 
     @Test
-    public void divideInfiniteByZeroTest() {
-        assertTrue(Double.isInfinite(numberDivider.divide(Double.POSITIVE_INFINITY, 0.0)));
-        assertTrue(Double.isInfinite(numberDivider.divide(Double.NEGATIVE_INFINITY, 0.0)));
+    public void divideFiniteStrictlyPositiveByZeroTest() {
+        assertTrue(Double.isInfinite(numberDivider.divide(+99.0, +0.0)));
+        assertTrue(Double.isInfinite(numberDivider.divide(+99.0, -0.0)));
+    }
+
+    @Test
+    public void divideFiniteStrictlyNegativeByZeroTest() {
+        assertTrue(Double.isInfinite(numberDivider.divide(-99.0, +0.0)));
+        assertTrue(Double.isInfinite(numberDivider.divide(-99.0, -0.0)));
+    }
+
+    @Test
+    public void dividePositiveInfinityByZeroTest() {
+        assertTrue(Double.isInfinite(numberDivider.divide(Double.POSITIVE_INFINITY, +0.0)));
+        assertTrue(Double.isInfinite(numberDivider.divide(Double.POSITIVE_INFINITY, -0.0)));
+    }
+
+    @Test
+    public void divideNegativeInfinityByZeroTest() {
+        assertTrue(Double.isInfinite(numberDivider.divide(Double.NEGATIVE_INFINITY, +0.0)));
+        assertTrue(Double.isInfinite(numberDivider.divide(Double.NEGATIVE_INFINITY, -0.0)));
     }
 
     @Test
     public void divideNaNByZeroTest() {
-        assertTrue(Double.isNaN(numberDivider.divide(Double.NaN, 0.0)));
+        assertTrue(Double.isNaN(numberDivider.divide(Double.NaN, +0.0)));
+        assertTrue(Double.isNaN(numberDivider.divide(Double.NaN, -0.0)));
     }
 
     @Test
     public void divideZeroByZeroTest() {
-        assertTrue(Double.isNaN(numberDivider.divide(0.0, 0.0)));
+        assertTrue(Double.isNaN(numberDivider.divide(+0.0, +0.0)));
+        assertTrue(Double.isNaN(numberDivider.divide(+0.0, -0.0)));
+        assertTrue(Double.isNaN(numberDivider.divide(-0.0, +0.0)));
+        assertTrue(Double.isNaN(numberDivider.divide(-0.0, -0.0)));
     }
 
     @Test
     public void divideByOneTest() {
-        for (int i = 0; i < 10; i++) {
-            assertEquals(numberDivider.divide(i, 1), i);
-        }
+        assertEquals(99, numberDivider.divide(99, 1));
     }
 
     @Test
     public void divideFinalNonZeroByItselfTest() {
-        for (int i = 0; i < 10; i++) {
-            assertEquals(numberDivider.divide(i, i), 1);
-        }
+        assertEquals(1, numberDivider.divide(99, 99));
+        assertEquals(1, numberDivider.divide(-99, -99));
+    }
+
+    @Test
+    public void divideFinalNonZeroByNegationTest() {
+        assertEquals(-1, numberDivider.divide(99, -99));
+        assertEquals(-1, numberDivider.divide(-99, 99));
+    }
+
+    @Test
+    public void divideOthersByOthersTest() {
+        assertEquals(0.7734375, numberDivider.divide(99, 128));
     }
 
 }
